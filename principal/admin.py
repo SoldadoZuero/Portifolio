ddc4c5a60feab6b5c921d171d9188dd4b3656520
SoldadoZuero <skeleton_fake@outlook.com>
2026-02-12 21:17:56 -0300

    Added model to services from BYS

--- principal/admin.py
+++ principal/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from principal.models import Servico
+
+
 # Register your models here.
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'preco', 'desconto')
+    search_fields = ('titulo',)
+    list_filter = ('preco',)
+    fieldsets = (
+        (None, {
+            'fields': ('titulo', 'descricao', 'preco', 'desconto')
+        }),
+    )
+
+
+admin.site.site_header = "Administração do Portfólio"
+admin.site.site_title = "Administração do Portfólio"
+admin.site.index_title = "Bem-vindo à Administração do Portfólio"
--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,16 @@
+from decimal import Decimal
+
 from django.db import models
 
 # Create your models here.
+
+
+class Servico(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(max_length=500, blank=True, null=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    desconto = models.DecimalField(
+        max_digits=10, decimal_places=2, default=Decimal('0.00'))
+
+    def __str__(self):
+        return self.titulo
